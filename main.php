1e45f883e3a7c73d6ff04871e3e85d0718dd7b2c
Nicole Furlan <nicolempitts@gmail.com>
2021-03-18 21:40:02 -0400

    Initial checkin

---
+++ main.php
@@ -0,0 +1,26 @@
+<section class="search">
+	<h2>Search</h2>
+	<p>You can also search for foods that are <a href="./safe" title="Safe"><span class="icon icon-thumbsup" role="img" title="Safe">&#x1F44D;</span>&nbsp;safe</a>&nbsp;or&nbsp;<a href="./unsafe" title="Unsafe"><span class="icon icon-thumbsdown" role="img" title="Unsafe">&#x1F44E;</span>&nbsp;unsafe</a></p>
+	<p class="bold small">Read&nbsp;some&nbsp;<a href="#tips" title="Tips">tips</a>&nbsp;before sharing any food with&nbsp;cats</p>
+	<label for="search">
+		<input id="search" type="search" autofocus="true" autocomplete="off" name="search" placeholder="enter food..." value="<?php echo (isset($project_data["pages"][$page]["url"]) && ($project_data["pages"][$page]["url"] !== "index")) ? $project_data["pages"][$page]["url"] : '' ?>" />
+	</label>
+	<div class="num-results"></div>
+	<div class="results"></div>
+</section>
+
+<section class="tips" id="tips">
+	<h2>Tips</h2>
+	<strong>When sharing food with cats, it is important to keep a few things in mind:</strong>
+	<ul>
+		<li><strong><b>Ask your veterinarian</b> before feeding your cat anything&#x2014;especially if they take <b>medication</b> or have any <b>medical conditions (e.g. diabetes, allergies, etc.)</b>.</strong> Some medication can interact with foods, so don't take any chances!</li>
+		<li><strong>Any foods cats are eating for the first time should be <b>introduced in small quantities</b>.</strong> Monitor them after they eat it, and if they experience any negative symptoms, don't feed it to them again.</li>
+		<li><strong>Any foods outside of your cat's normal diet should make up <b>less than 10% of their daily food intake</b>.</strong> For example, if your cat eats 1 cup of food per day, they shouldn't eat more than &#x2152; cup of treats (e.g. a few blueberries). Any foods fed to cats in large amounts can lead to digestive upset, so moderation is truly key.</li>
+		<li><strong>Any foods shared with cats should be cut into <b>bite-size pieces, with all choking hazards removed (e.g. pits, peels, seeds)</b>.</strong> These choking hazards can even become obstructed in your cats gastrointestinal tract. Never share these things with cats, even if they are perfectly normal for humans to eat.</li>
+		<li><strong>Cats shouldn't eat foods with <b>added salt, oils, sugar, seasonings, sauces, or any other variations</b>.</strong> These additions, while delicious, often contain ingredients that are harmful, if not <strong>toxic</strong> to cats, such as garlic, onions, or even xylitol. Cats should not eat foods that were prepared with these ingredients, either.</li>
+		<li id="highfat"><strong>Cats should only eat foods that are high in fat in moderation,</strong> even if they are generally considered healthy for us. For dogs, eating large amounts of fat can cause gastrointestinal upset and even pancreatitis</strong>, an inflammation of the pancreas which can range from mild to severe, and can require emergency veterinary treatment. For cats, however, <a href="https://www.hillspet.com/cat-care/healthcare/pancreatitis-in-cats-symptoms-treatment" target="_blank" rel="noopener"> the link between too much fat and pancreas problems in cats is still being studied</a>. Out of an abundance of caution, cats should only eat limited amounts of fatty foods.</li>
+		<li id="processed"><strong>Cats shouldn't eat <b>processed foods</b> such as baked foods, boxed snacks, fast food, etc.</strong> Aside from being unhealthy and high in fat, they often conceal ingredients that may be troublesome or even <strong>toxic</strong> for cats, such as a sweetener called xylitol. Be sure to thoroughly <strong>read the ingredients</strong> of any processed foods you share with your cat, and if you can't determine the safety level of each ingredient, do not share it with your cat.</li>
+		<li id="canned"><strong>Cats shouldn't eat <b>canned</b> versions of foods (e.g. fruits, vegetables, etc.), due to their added preservatives</strong>&nbsp;&#x2014;including excess salt&#x2014; and sugar, which can be harmful to cats (and humans!). Choose fresh or even frozen versions instead.</li>
+		<li id="carbohydrate"><strong>Cats lack the enzymes required to digest carbohydrates.</strong> Unlike humans and dogs, cats are carnivorous, and do not digest sugars the same way we do. Over time, feeding cats carbohydrates and sugary foods will cause digestive issues, and can also lead to obesity and diabetes, and therefore, their diets should not exceed 2% of carbohydrates or sugars.</li>
+	</ul>
+</section>
