8bbe0833e389cdd567b67015c9c4a76e6add593f
nikkifurls <nicolempitts@gmail.com>
2023-08-10 23:30:26 -0400

    Changes notification to `<dialog>` and changes `.social` to be center justified on mobile

--- sw.js
+++ sw.js
@@ -1,19 +1,20 @@
-const cacheName = "cache67";
+const cacheName = "cache68";
 const cacheFiles = [
 	"/",
-	"disclaimer.html",
-	"img/logo_nav.svg",
-	"index.html",
 	"manifest.json",
-	"privacy-policy.html",
-	"terms-and-conditions.html",
 	"data.json",
+	"img/logo_header.svg",
+	"img/logo_nav.svg",
 	"img/icon-thumbs-up.png",
 	"img/icon-thumbs-down.png",
 	"ads.txt",
 	"sellers.json",
 	"safe.html",
 	"unsafe.html",
+	"index.html",
+	"disclaimer.html",
+	"privacy-policy.html",
+	"terms-and-conditions.html",
 	"acai.html",
 	"alcohol.html",
 	"beer.html",
@@ -296,7 +297,7 @@ const cacheFiles = [
 	"fonts/fontawesome/webfonts/fa-solid-900.ttf",
 	"fonts/fontawesome/webfonts/fa-solid-900.woff",
 	"fonts/fontawesome/webfonts/fa-solid-900.woff2",
-	"js/bundle.3888c41db6069b85b898.js",
+	"js/bundle.d40f670f7f12e800afa7.js",
 	"img/android-chrome-36x36.png", // Favicon, Android Chrome M39+ with 0.75 screen density
 	"img/android-chrome-48x48.png", // Favicon, Android Chrome M39+ with 1.0 screen density
 	"img/android-chrome-72x72.png", // Favicon, Android Chrome M39+ with 1.5 screen density
